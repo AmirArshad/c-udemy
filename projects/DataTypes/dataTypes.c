5348f354c250d3f6fbae01d36796dc9e947ee94e
Amir Arshadnejad <amirshareh@gmail.com>
2022-12-30 17:05:58 +0000

    dataTypes sample code

---
+++ projects/DataTypes/dataTypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int amir = 0, patrick = 0, michal = 12;
+
+    amir = 69;
+    patrick = 89;
+    michal = 120;
+
+    amir = patrick + michal;
+
+    printf("%d",amir);
+
+    return 0;
+}
